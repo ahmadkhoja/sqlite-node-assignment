23fc0b92523834ecbf6cf4d62cac4273db09cd43
ahmadkhoja <ahmadkhoja96@gmail.com>
2018-05-10 22:11:30 +0300

    node with sqlite project is finished

--- back/connect.js
+++ back/connect.js
@@ -1,5 +1,23 @@
+const port = 8888
+const express = require('express')
+const bodyParser = require('body-parser')
+const server = express()
 const sqlite3 = require('sqlite3').verbose();
 
+server.use(bodyParser.urlencoded({ extended: true }));
+server.use(bodyParser.json());
+server.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+server.listen(port,(err)=>{
+  if(err){
+      console.log('cannot connect to the server')
+  }else{
+      console.log(`listining to port ${port}`)
+  }
+})
 // open database in memory
 let db = new sqlite3.Database('./db/mydatabase.db', sqlite3.OPEN_READWRITE, (err) => {
   if (err) {
@@ -7,59 +25,159 @@ let db = new sqlite3.Database('./db/mydatabase.db', sqlite3.OPEN_READWRITE, (err
   }
   console.log('Connected to the database named mydatbase');
 });
-//selecting all data from database
-let sql = `SELECT * FROM movies ORDER BY id`;
-db.all(sql, [], (err, rows) => {
+
+server.get('/', ( req, res ) => {
+  db.all('SELECT * FROM movies', [], (err, rows) => {
     if (err) {
       throw err;
     }
-    rows.forEach((row) => {
-      console.log(row);
-    });
-});
-let id = 6
-let name = 'lord of the rings'
-let genre = 'action'
+      res.send({movies:rows})
+  });
+})
 
-// //adding new data to the database
-// sql = `insert into movies ('id','name','genre') values ('`+id+`','`+name+`','`+genre+`')`;
-// db.all(sql, [], (err, rows) => {
-//     if (err) {
-//       throw err;
-//     }
-//     rows.forEach((row) => {
-//       console.log(row);
-//     });
-// });
-
-// //updating existing data in the database
-// let data = ['Tarazan','Starwars'];
-// let update_sql = `UPDATE movies
-//             SET name = ?
-//             WHERE name = ?`;
- 
-// db.run(update_sql, data, function(err) {
-//   if (err) {
-//     return console.error(err.message);
-//   }
-//   console.log(update_sql)
-//   console.log(`Row(s) updated: ${this.changes}`);
-// });
-
-// // deleting data from the database
-// id = 2;
-// // delete a row based on id
-// db.run(`DELETE FROM movies WHERE rowid=?`, id, function(err) {
-//   if (err) {
-//     return console.error(err.message);
-//   }
-//   console.log(`Row(s) deleted ${this.changes}`);
-// });
-
-// close the database connection
-db.close((err) => {
+server.post('/create_movie', (req, res) => {
+  console.log('you posted to /create_movie');
+  const { name, genre } = req.body
+  if(!name){
+    res.send('name is required')
+    return 
+  }
+  if(!genre){
+    res.send('genre is required')
+    return
+  }
+  if(!name || !genre){
+    return res.send({error:'blah'})
+  }
+  db.run('INSERT INTO movies (name, genre) VALUES (?, ?)', [ name, genre ], function(err) {
     if (err) {
-      return console.error(err.message);
+      return res.send(err.message);
     }
-    console.log('Close the database connection.');
+    // get the last insert id
+    res.redirect('/')
+  });
+  // insert(name,genre)
+  // res.send(req.body)
 });
+server.get('/edit/:id', ( req, res ) => {
+  const id = req.params.id
+  db.get('SELECT * FROM movies WHERE id = ?', id, ( err, movie ) => {
+    if(err){
+      return res.send(err.message)
+    }
+    if(!movie){
+      return res.send('movie not found')
+    }
+    const form = `
+    <form action="/update/`+id+`">
+      <input value="`+movie.Name+`" type="text" name="name"  placeholder="name"/>
+      <input value="`+movie.Genre+`" type="text" name="genre" placeholder="genre"/>
+      <input type="submit" value="ok"/>
+    </form>`
+    res.send(form)
+    // res.redirect('//localhost:3000/')
+    // res.send({movie})
+  })
+})
+server.get('/update/:id', ( req, res ) => {
+  const id = req.params.id
+  const name = req.query.name
+  const genre = req.query.genre
+  db.run('UPDATE movies SET name = ?, genre = ? WHERE id = ?', [ name, genre, id ], function(err) {
+    if (err) {
+      return res.send(err.message);
+    }
+    res.redirect('/edit/'+id)
+  });
+})
+
+// localhost:3000/delete/<id>
+server.get('/delete/:id', ( req, res ) => {
+  const id = req.params.id
+  db.run(`DELETE FROM movies WHERE id = ?`, id, function(err) {
+    if (err) {
+      return res.send(err.message);
+    }
+    if(this.changes === 0){
+      return res.send("I didn't delete anything")
+    }
+    res.redirect('//localhost:3000/')
+  });
+})
+
+// list()
+// console.log(process.argv)
+//Do it from command line
+// const args = process.argv.slice(2)
+// const command = args[0]
+// const name = args[1]
+// const genre = args[2]
+
+// if(command === '-a' || command === '-add'){
+//   if(!name){
+//     console.error('you have to provide a name')
+//   }
+//   if(!genre){
+//     console.error('you have to provide a genre')
+//   }
+//   else if(command === 'list'){
+//     list()
+//   }
+  
+//   insert(name,genre)
+//   // console.log(args)
+//   console.log(' name: ', name,' genre: ', genre)
+  
+// }
+
+// insert('ifeuwb','efihb')
+
+
+  // server.get('/delete/:id',(req,res) => {
+  //   const id = req.params.id
+  //   db.run(`DELETE FROM movies WHERE id=?`, id, function(err) {
+  //     if (err) {
+  //       return res.send(err.message);
+  //     }
+  //     if(this.changes === 0 ){
+  //       return res.send("I didn't delete anything");        
+  //     }
+  //     res.redirect('/')
+  //   });
+  // })
+
+  //add (not finished yet)
+  // server.get('/add/:id',(req,res) => {
+  //   const name = req.query.name
+  //   const genre = req.query.genre
+  // sql = `insert into movies ('id','name','genre') values ('30','`+name+`','`+genre+`')`;
+  // db.all(sql, [], (err, rows) => {
+  //     if (err) {
+  //       throw err;
+  //     }
+  //     rows.forEach((row) => {
+  //       console.log(row);
+  //     });
+  // res.redirect('/')
+    // });
+  // })
+
+  // server.get('/form', (req,res) => {
+  //   const form = `
+  //     <form action="/add">
+  //     <input type="text" name="name"  placeholder="name"/>
+  //     <input type="text" name="genre" placeholder="genre" />
+  //     <input type="submit" />
+  //     </form>
+  //   `;
+  // })
+
+  //delete not finished yet
+  // db.all('SELECT * FROM movies ', [], (err, rows) => {
+  //   if (err) {
+  //     throw err;
+  //   }
+  // rows.forEach((row) => {
+  //   console.log(row.name,'/t',row.genre)+`<a href="/delete/"+`+row.id+`+'>'+</a>' `;
+  // });
+  // });
--- front/src/App.js
+++ front/src/App.js
@@ -1,19 +1,79 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import fetch from 'node-fetch'
+import './App.css'
 
+const Movie = ({id,name,genre,removeMovie}) => {
+  return(
+    <div>
+      <h2>Movie Name:{name}</h2>
+      <h3>Genre: {genre}</h3>
+      <a className="crud" href={'//localhost:8888/edit/' + id}>edit</a>
+      <a className="crud" href={'//localhost:8888/delete/' + id}>delete</a>
+      <hr/>
+    </div>
+  )
+}
 class App extends Component {
+  state = {
+    movies:[]
+  }
+  componentDidMount = () => {
+    fetch("//localhost:8888/")
+    .then((res) => res.json())
+    .then((res) => this.setState({movies:res.movies}))
+  }
+  handleSubmit(e){
+    e.preventDefault();
+    let reqBody = {
+      name: e.target.movie_name.value,
+      genre: e.target.movie_genre.value
+    };
+    // console.log(reqBody)
+    fetch("//localhost:8888/create_movie", {
+      method: "POST",
+      body: JSON.stringify(reqBody),
+      headers: {
+          "Content-Type": "application/json"
+      }
+    })
+    .then((res) => res.json())
+    .then((res) => console.log(res))
+     e.target.movie_name.value = ''
+     e.target.movie_genre.value = ''
+  }
+  // removeMovie = (movie) => {
+  //   const movies = this.state.movies
+  //   const index = movies.indexOf(movie)
+  //   console.log(index)
+  //   if(index<0){
+  //     return;
+  //   }
+  //   movies.splice(index,1)
+  // }
+  renderMovies = () => {
+    return(
+      this.state.movies.map((movie) => <Movie name={movie.Name} genre={movie.Genre} key={movie.ID} removeMovie={() => this.removeMovie(movie)} id={movie.ID}/>)
+    )
+  }
   render() {
+    const movie_list = this.renderMovies()
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+              <form onSubmit={this.handleSubmit} className="movie-form">
+                <div>
+                  <label>Name of the Movie:</label><br/>
+                  <input type="text" name="movie_name" required/>
+                </div>
+                <div>
+                  <label>Movie Genre:</label><br/>
+                  <input type="text" name="movie_genre" required/>
+                </div>
+                <div>
+                  <input type="submit" value="Submit"/>
+                </div>
+              </form>
+              {movie_list}
+        </div>
     );
   }
 }
